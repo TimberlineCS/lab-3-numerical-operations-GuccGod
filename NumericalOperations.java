5f31b388957e78b4f171a1b23524072468d89262
GuccGod <coprivnicool@gmail.com>
2020-09-02 17:00:05 +0000

    Added pt 2

---
+++ NumericalOperations.java
@@ -0,0 +1,30 @@
+public class NumericalOperations{
+public static void main(String args[]){
+
+double d1 = 37.9;
+double d2 = 1004.128;
+int i1 = 12;
+int i2 = 18;
+/*Predictions: 
+Q1=1
+Q2=40
+Q3=Honestly not sure
+Q4=-1397.4
+Q5=-1345.4
+*/
+System.out.println(57.2*(i1/i2)+1);
+System.out.println(57.2*((double)i1/i2)+1);
+System.out.println(15–i1(3*d1)+4);
+System.out.println(15 – i1 ((int)d1 3) + 4);
+System.out.println(15 – i1 (int)(d1 3) + 4);
+
+
+int love = 1234;
+String l = love.substring(0);
+String o = love.substring(1);
+String v = love.substring(2);
+String e = love.substring(3);
+System.out.println(e+v+o+l);
+
+
+}}
